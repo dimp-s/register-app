d9a14051411a7ab68370761f270949d91fd3e3e1
dimp-s <dipesh0221@gmail.com>
2025-05-16 13:41:45 +0545

    added dashboard template

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,29 +1,29 @@
-import { Component, OnInit, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { AuthService } from '../../services/auth.service';
+// import { Component, OnInit, inject } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { RouterModule } from '@angular/router';
+// import { MatCardModule } from '@angular/material/card';
+// import { MatButtonModule } from '@angular/material/button';
+// import { AuthService } from '../../services/auth.service';
 
-@Component({
-  selector: 'app-dashboard',
-  standalone: true,
-  imports: [CommonModule, RouterModule, MatCardModule, MatButtonModule],
-  templateUrl: './dashboard.component.html',
-})
-export class DashboardComponent implements OnInit {
-  userData: any = null;
+// @Component({
+//   selector: 'app-dashboard',
+//   standalone: true,
+//   imports: [CommonModule, RouterModule, MatCardModule, MatButtonModule],
+//   templateUrl: './dashboard.component.html',
+// })
+// export class DashboardComponent implements OnInit {
+//   userData: any = null;
 
-  authService = inject(AuthService);
+//   authService = inject(AuthService);
 
-  ngOnInit() {
-    const userId = this.authService.getUserIdFromToken();
-    console.log(userId);
-    if (userId) {
-      this.authService.getDashboardData(userId).subscribe({
-        next: (data) => (this.userData = data),
-        error: (err) => console.error('Failed to load dashboard data', err),
-      });
-    }
-  }
-}
+//   ngOnInit() {
+//     const userId = this.authService.getUserIdFromToken();
+//     console.log(userId);
+//     if (userId) {
+//       this.authService.getDashboardData(userId).subscribe({
+//         next: (data) => (this.userData = data),
+//         error: (err) => console.error('Failed to load dashboard data', err),
+//       });
+//     }
+//   }
+// }
